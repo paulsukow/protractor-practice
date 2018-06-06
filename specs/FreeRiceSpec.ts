98d2d9102017a5372ff9c0aac20267acb45eb34a
paulsukow <paul.sukow@gmail.com>
2018-06-06 08:54:48 -0600

    remove unneeded browser.element and replaced with element

--- specs/FreeRiceSpec.ts
+++ specs/FreeRiceSpec.ts
@@ -11,12 +11,10 @@ describe('free rice example', function () {
    });
 
    it('should be able to change to the multiplication table under the math subject', function () {
-       browser.waitForAngularEnabled(false);
+       element(by.linkText('SUBJECTS')).click();
+       element(by.linkText('Multiplication Table')).click();
 
-       browser.element(by.linkText('SUBJECTS')).click();
-       browser.element(by.linkText('Multiplication Table')).click();
-
-       browser.element(by.className('subject-title')).getText().then( function (tableName) {
+       element(by.className('subject-title')).getText().then( function (tableName) {
            expect(tableName).toEqual('Multiplication Table')
        });
    });
@@ -27,17 +25,17 @@ describe('free rice example', function () {
        let multiplier;
        let product;
 
-       browser.element(by.className('question-link')).getText().then( function (problemText) {
+       element(by.className('question-link')).getText().then( function (problemText) {
            factors = problemText.split(' x ');
            multiplicand = parseInt(factors[0]);
            multiplier = parseInt(factors[1]);
 
            product = multiplier * multiplicand;
 
-           browser.element(by.linkText(product.toString())).click();
+           element(by.linkText(product.toString())).click();
        });
 
-       browser.element(by.css('#game-status td')).getText().then( function (text) {
+       element(by.css('#game-status td')).getText().then( function (text) {
            expect(text).toContain('Correct! ' + multiplicand + ' x ' + multiplier + ' = ' + product)
        });
    });
